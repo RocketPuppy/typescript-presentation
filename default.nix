7440b1c28ad3588db68c1e6daa0e6d84964d562d
Daniel Wilson-Thomas <daniel@mojotech.com>
2018-02-06 21:30:22 -0500

    Initial infrastructure

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ nixpkgsFn ? import ./nixpkgs.nix
+, system ? null }:
+let nixpkgs = nixpkgsFn ({
+      # extra config goes here
+    } // ( if system == null then {} else { inherit system; } ));
+in
+nixpkgs.stdenv.mkDerivation {
+  name = "typescript-presentation";
+  buildInputs = with nixpkgs; [ nodejs yarn git ];
+  src = "./";
+
+  builder = builtins.toFile "builder.sh" ''
+    echo "Use this derivation with nix-shell only"
+
+    exit 1
+  '';
+
+  shellHook = ''
+    cd $src
+    yarn
+    export PATH=$src/node_modules/.bin:$PATH
+  '';
+}
+
+
